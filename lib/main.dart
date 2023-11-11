80b56945b57e51415e9f42b1df069647bc64e170
Krixnaas <krishna.shrestha@racingandsports.com>
2023-11-11 13:40:06 +1100

    added helper functions, utilities, text_string, image_string  and constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:app/utils/theme/theme.dart';
+import 'app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-    );
-  }
 }
