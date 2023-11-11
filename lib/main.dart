584009cd29436359e95d02f016007a4c605a0e7a
Krixnaas <krishna.shrestha@racingandsports.com>
2023-11-11 12:21:26 +1100

    custom theme and font addded

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,20 @@
+import 'package:app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
+void main() {
+  runApp(const App());
+}
 
-void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
+class App extends StatelessWidget {
+  const App({super.key});
 
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+    );
+  }
 }
---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ACheckBoxTheme {
+  ACheckBoxTheme._();
+
+  static CheckboxThemeData lightCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith(
+      (states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      },
+    ),
+    fillColor: MaterialStateProperty.resolveWith(
+      (states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.black;
+        }
+      },
+    ),
+  );
+
+  static CheckboxThemeData darkCheckBoxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith(
+      (states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.white;
+        } else {
+          return Colors.black;
+        }
+      },
+    ),
+    fillColor: MaterialStateProperty.resolveWith(
+      (states) {
+        if (states.contains(MaterialState.selected)) {
+          return Colors.blue;
+        } else {
+          return Colors.black;
+        }
+      },
+    ),
+  );
+}
