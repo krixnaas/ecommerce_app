d92fad0c853f78988c640e4e3151a95d89c686e4
Krixnaas <krishna.shrestha@racingandsports.com>
2023-11-12 14:26:57 +1100

    custom app bar added

---
+++ lib/common/widgets/app_bar.dart
@@ -0,0 +1,44 @@
+import 'package:app/utils/constants/sizes.dart';
+import 'package:app/utils/device/device_utility.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar(
+      {super.key,
+      this.title,
+      this.showBackArrow = false,
+      this.leadingIcon,
+      this.actions,
+      this.leadingOnPressed});
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget>? actions;
+  final VoidCallback? leadingOnPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: Sizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: true,
+        leading: showBackArrow
+            ? IconButton(
+                onPressed: () => Get.back(),
+                icon: const Icon(Iconsax.arrow_left))
+            : leadingIcon != null
+                ? IconButton(
+                    onPressed: leadingOnPressed, icon: Icon(leadingIcon))
+                : null,
+        title: title,
+        actions: actions,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(DeviceUtils.getAppBarHeight());
+}
